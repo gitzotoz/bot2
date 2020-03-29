ad0628874a1e9fefda41f8bb96006e4e3437a4a4
user <gzotoz>
2020-03-29 18:19:34 +0300

    bot

---
+++ src/main/java/Bot.java
@@ -0,0 +1,31 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Bot extends TelegramLongPollingBot {
+
+    public void onUpdateReceived(Update update) {
+
+    }
+
+    public String getBotUsername() {
+        return null;
+    }
+
+    public String getBotToken() {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new Bot());
+        } catch (TelegramApiException e){
+            e.printStackTrace();
+        }
+    }
+
+}
