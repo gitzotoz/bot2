97897346a54541b85acb54be046719ee42f9666d
gitzotoz <123@213>
2020-04-01 09:43:23 +0300

    bot2

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,12 +1,35 @@
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class Bot extends TelegramLongPollingBot {
 
     public void onUpdateReceived(Update update) {
+
+        Message message=update.getMessage();
+
+        SendMessage sendMessage= new SendMessage();
+        if (message!=null && message.hasText()){
+            switch (message.getText()){
+                case "/help":
+                    try {
+                        execute(sendMessage.setText(message.getText() + "how can i help"));
+                    } catch (TelegramApiException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case "/settings":
+                    try {
+                        execute(sendMessage.setText(message.getText() + "What are we going to customize?"));
+                    } catch (TelegramApiException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+            }
 
     }
 
